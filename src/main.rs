fc57a707698da9fa49c9e3bf09fd63b5da44c469
Paul Houghton <paulirotta@gmail.com>
2019-04-20 16:39:52 +0300

    Refactored- added main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
 // Perform a forward FFT of size 1234
 
+
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
-use rustfft::FFTplanner;
+use rustfft::{FFTplanner, FFT};
+use std::sync::Arc;
 
 pub fn main() {
     const WINDOW: usize = 1024;
-    const SAMPLE_RATE: f32 = 16_000.0;
 
-    let mut input: Vec<Complex<f32>> = vec![Complex::zero(); WINDOW];
-    let mut output: Vec<Complex<f32>> = vec![Complex::zero(); WINDOW];
+    let time_domain: Vec<f32> = vec![0.0; WINDOW];
 
     let mut planner = FFTplanner::new(false);
     let fft = planner.plan_fft(WINDOW);
-    fft.process(&mut input, &mut output);
 
-    let short_term_energy(time_domain);
-    let peak_freq = bin_to_freq(peak_bin(&output), WINDOW, SAMPLE_RATE);
-    let sfm = spectral_flatness_measure(&output);
+    let vad = VadFrame::new(&time_domain, &fft);
+
+    println!("vad silence: {:?}", vad);
 }
 
-fn short_term_energy(time_domain: Vec<f32>) -> f32 {
+
+#[derive(Debug)]
+pub struct VadFrame {
+    pub energy: f32,
+    pub dominant_freq: f32,
+    pub spectral_flatness_measurement: f32,
+}
+
+impl<'a> VadFrame {
+    pub fn new(time_domain: &'a [f32], fft: &Arc<FFT<f32>>) -> VadFrame {
+        const WINDOW: usize = 1024;
+        const SAMPLE_RATE: f32 = 16_000.0;
+
+        assert!(time_domain.len() == WINDOW);
+
+        let mut input: Vec<Complex<f32>> = Vec::with_capacity(WINDOW);
+        let mut output: Vec<Complex<f32>> = vec![Complex::zero(); WINDOW];
+
+        for i in 0..WINDOW {
+            input.push(Complex::new(time_domain[i], 0.0));
+        }
+
+        let energy = short_term_energy(time_domain);
+        fft.process(&mut input, &mut output);
+        let dominant_freq = bin_to_freq(peak_bin(&output), WINDOW, SAMPLE_RATE);
+        let spectral_flatness_measurement = measure_spectral_flatness(&output);
+
+        VadFrame {
+            energy: energy,
+            dominant_freq: dominant_freq,
+            spectral_flatness_measurement: spectral_flatness_measurement,
+        }
+    }
+}
+
+fn short_term_energy(time_domain: &[f32]) -> f32 {
     let mut sum = 0.0;
 
     for val in time_domain.iter() {
         let v = val.abs();
-        sum = sum + v*v;
+        sum = sum + v * v;
     }
 
     sum / time_domain.len() as f32
 }
 
-fn peak_bin(frame: &Vec<Complex<f32>>) -> usize {
+fn peak_bin(frame: &[Complex<f32>]) -> usize {
     let mut max_val = std::f32::MIN;
     let mut max_bin = 0;
 
@@ -49,7 +83,7 @@ fn bin_to_freq(bin: usize, window_size: usize, sample_rate: f32) -> f32 {
     (bin as f32 / window_size as f32) * sample_rate
 }
 
-fn geometric_mean(vec: &Vec<Complex<f32>>) -> f32 {
+fn geometric_mean(vec: &[Complex<f32>]) -> f32 {
     let mut mean: f32 = 1.0;
 
     for v in vec {
@@ -59,7 +93,7 @@ fn geometric_mean(vec: &Vec<Complex<f32>>) -> f32 {
     mean.powf(1.0 / vec.len() as f32)
 }
 
-fn arithmetic_mean(vec: &Vec<Complex<f32>>) -> f32 {
+fn arithmetic_mean(vec: &[Complex<f32>]) -> f32 {
     let mut mean: f32 = 0.0;
 
     for v in vec {
@@ -69,6 +103,7 @@ fn arithmetic_mean(vec: &Vec<Complex<f32>>) -> f32 {
     mean / vec.len() as f32
 }
 
-fn spectral_flatness_measure(fft: &Vec<Complex<f32>>) -> f32 {
+fn measure_spectral_flatness(fft: &[Complex<f32>]) -> f32 {
     10.0 * (geometric_mean(fft) / arithmetic_mean(fft)).log(10.0)
 }
+
